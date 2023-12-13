698b96add9f345a70eabd0d5f5235cbb16d74c1e
Nicolaus Bunch <77365237+Coldwel@users.noreply.github.com>
2023-12-13 08:31:15 -0700

    Update main.py
    Updated SOFTWARE_VERSION, should be 0.1.1

--- main/main.py
+++ main/main.py
@@ -5,7 +5,7 @@ from controller import SearchController
 from view import SearchView
 from model import PUBLIC_KEY, PRIVATE_KEY
 
-SOFTWARE_VERSION = "0.1.0"
+SOFTWARE_VERSION = "0.1.1"
 
 with open("config.json") as f:
     config = json.load(f)
