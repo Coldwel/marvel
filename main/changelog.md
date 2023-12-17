0b8447dcb0532400475dbe61a977f18511a1d804
Nick <nicolaus.bunch@gmail.com>
2023-12-16 21:26:10 -0700

    Revert "Alpha 0.1.2 release"
    This reverts commit cd1f847ef8de91bd4290bcaa1c86563bf17ac164.

--- main/changelog.md
+++ main/changelog.md
@@ -1,34 +1,11 @@
-# Changelog
-
-### [Version 0.1.2]
-
-### [16 December 2023]
-
-- **Feature**: Asynchronous Fetching of Comics Data
-  - Introduced an asynchronous function `fetch_comics_data` for fetching data from the comics API using `aiohttp`.
-  - The function supports making asynchronous GET requests to the comics API with specified headers and parameters.
-  - This enhances the application's performance by allowing asynchronous handling of API requests.
-
-- **Project Management**
-  - Organized tasks and TODO items in `TODO.md`.
-  - Updated and reorganized changelog for better clarity.
-  - Updated the 'README.md' for a more detailed description of the project.
-
-### [14-15 December 2023]
-
-- **Method Name Change**: Renamed `show_comic_window` method to `retrieve_and_display_comics` for clarity in functionality.
-- **Scrollbar Enhancement**: Updated the scrollbar in `display_comics_list` to fix its placement issue. It is now attached to comics canvas instead of comics window, addressing the tiny scrollbar in the lower right corner.
-- **Comics List Descriptions**: Added descriptions to the comics list to display the API description of each comic.
-
-### [12 December 2023]
-
-- **Thumbnail Images**: Implemented thumbnail images for comics displayed in the list after pressing the 'comics' button.
-- **Homescreen Improvement**: Added a static Marvel logo to the homescreen for a more visually appealing layout.
-  - Created a new static image method to handle the logo.
-- **Layout Adjustment**: Changed the homescreen layout to position the search bar under the logo.
-  - Created the `setup_search_frame()` method to manage search elements.
-  - Placed search elements using `place()` on both the homescreen and results screen.
-  - Set focus to the `query_entry()` text box on the homescreen for user convenience.
-  - Created a frame to organize search results for better layout control.
-- **Scroll Wheel Functionality**: Enabled scroll wheel functionality anywhere the cursor was idle.
-- **Debugging Enhancements**: Added debugging `print()` statements to the `search_api()` method for better troubleshooting.
+12 December 2023, v0.1.1
+- implemented thumbnail images of comics shown in list of comics after you press the 'comics' button.
+- added static Marvel logo to homescreen, so it doesn't look so empty.
+  - made a new static image method to handle that
+- changed the layout of the homescreen so search bar is under logo.
+- created new setup_search_frame() method to handle search elements.
+  - had to place() search elements in both homescreen and results screen.
+  - homescreen now uses focus() to start inside the query_entry() text box.
+  - created a frame to place the search results on for better layout control.
+- made scroll wheel function anywhere cursor was idle.
+- added debugging print() statements to search_api() method.
--- main/view.py
+++ main/view.py
@@ -5,9 +5,6 @@ import io
 from PIL import Image, ImageTk
 from cache import cache
 from model import PUBLIC_KEY, PRIVATE_KEY, MarvelAPI
-import asyncio
-import aiohttp
-import time
 
 # Define custom colors
 PRIMARY_COLOR = "#ed1d24"
@@ -21,7 +18,6 @@ HEADER_FONT = ("Helvetica Neu", 18, "bold")
 LABEL_FONT = ("Helvetica Neu", 14)
 BUTTON_FONT = ("Helvetica Neu", 12, "bold")
 VERSION_FONT = ("Helvetica Neu", 10, "bold")
-SMALL_FONT = ("Helvetica Neu", 10)
 
 
 class SearchView:
@@ -154,12 +150,12 @@ class SearchView:
                 hero_name.pack(pady=5, anchor="w")
 
                 hero_desc = tk.Label(hero_info_frame, text=result["description"], bg=BG_COLOR, fg=TEXT_COLOR,
-                                     font=LABEL_FONT, wraplength=450)
+                                     font=LABEL_FONT, wraplength=300)
                 hero_desc.pack(pady=5, anchor="w")
 
                 comics_button = tk.Button(hero_info_frame, text="Comics", bg=ACCENT_COLOR, fg=SECONDARY_COLOR,
                                           font=BUTTON_FONT,
-                                          command=lambda url=result["comics_url"]: self.retrieve_and_display_comics(url))
+                                          command=lambda url=result["comics_url"]: self.show_comics_window(url))
                 comics_button.pack(pady=5, anchor="w")
 
                 # Bind mouse wheel event to the canvas
@@ -168,28 +164,7 @@ class SearchView:
                 # Set a fixed size for the results window
                 self.root.geometry("800x600")
 
-    async def fetch_comics_data(self, comics_url, timestamp, md5_hash):
-        headers = {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        }
-        params = {
-            "ts": timestamp,
-            "apikey": PUBLIC_KEY,
-            "hash": md5_hash,
-            "format": "comic",
-            "formatType": "comic",
-            "noVariants": "True",
-            "limit": 25,
-            "orderBy": "-focDate"
-        }
-        async with aiohttp.ClientSession() as session:
-            async with session.get(comics_url, headers=headers, params=params) as response:
-                data = await response.json()
-
-        return data
-
-    def retrieve_and_display_comics(self, comics_url):
+    def show_comics_window(self, comics_url):
         if comics_url in cache:
             comics_list = cache[comics_url]
             self.display_comics_list(comics_list)
@@ -197,16 +172,29 @@ class SearchView:
             timestamp = "1"
             md5_hash = self.controller.generate_hash(timestamp)
 
-            loop = asyncio.get_event_loop()
-            data = loop.run_until_complete(self.fetch_comics_data(comics_url, timestamp, md5_hash))
+            headers = {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+            params = {
+                "ts": timestamp,
+                "apikey": PUBLIC_KEY,
+                "hash": md5_hash,
+                "format": "comic",
+                "formatType": "comic",
+                "noVariants": True,
+                "limit": 100,
+                "orderBy": "-focDate"
+            }
+            response = requests.get(comics_url, headers=headers, params=params)
+            data = response.json()
 
             comics_list = []
             if data["data"]["total"] > 0:
                 for result in data["data"]["results"]:
                     comic_item = {
                         "title": result["title"],
-                        "thumbnail_url": result["thumbnail"]["path"] + "/portrait_uncanny." + result["thumbnail"]["extension"],
-                        "description": result["description"]
+                        "thumbnail_url": result["thumbnail"]["path"] + "/portrait_uncanny." + result["thumbnail"]["extension"]
                     }
                     comics_list.append(comic_item)
 
@@ -217,8 +205,6 @@ class SearchView:
                 self.show_error("No comics found.")
 
     def display_comics_list(self, comics_list):
-        # Testing methods runtime
-        start = time.time()
         # create a new window to show the comics list
         comics_window = tk.Toplevel(self.root)
         comics_window.title("Comics List")
@@ -233,8 +219,6 @@ class SearchView:
         comics_frame = tk.Frame(comics_canvas, bg=BG_COLOR)
         comics_canvas.create_window((0, 0), window=comics_frame, anchor=tk.NW)
 
-        # Initialize counter for row index
-        row_index = 0
         # Display the comics list in the Frame
         for comic_item in comics_list:
             # Fetch the thumbnail URL from the comic information
@@ -245,29 +229,20 @@ class SearchView:
                 image_bytes = requests.get(thumbnail_url).content
                 image_data = io.BytesIO(image_bytes)
                 img = Image.open(image_data)
-                img = img.resize((100, 150))  # Adjust the size as needed
+                img = img.resize((150, 200))  # Adjust the size as needed
                 img = ImageTk.PhotoImage(img)
 
                 # Display the thumbnail image within the Canvas
                 thumbnail_label = tk.Label(comics_frame, image=img, bg=BG_COLOR)
                 thumbnail_label.image = img
-                thumbnail_label.grid(row=row_index, column=0, padx=5, pady=5, sticky=tk.W)
+                thumbnail_label.grid(row=comics_list.index(comic_item), column=0, padx=5, pady=5, sticky=tk.W)
 
             # Display the comic title
             title_label = tk.Label(comics_frame, text=comic_item['title'], bg=BG_COLOR, fg=TEXT_COLOR, font=LABEL_FONT)
-            title_label.grid(row=row_index, column=1, padx=5, pady=5, sticky=tk.W)
-            # Display the comic description
-            des_label = tk.Label(comics_frame, text=comic_item['description'], bg=BG_COLOR, fg=TEXT_COLOR,
-                                 font=SMALL_FONT, wraplength=500)
-            des_label.grid(row=row_index + 1, column=1, padx=5, pady=5, sticky=tk.W)
-
-            row_index += 2
-
-        comics_canvas.update_idletasks()  # Update to calculate the canvas size
-        comics_canvas.config(scrollregion=comics_canvas.bbox('all'))  # Set the initial scroll region
+            title_label.grid(row=comics_list.index(comic_item), column=1, padx=5, pady=5, sticky=tk.W)
 
         # Add a vertical scrollbar for the Canvas
-        scrollbar = tk.Scrollbar(comics_canvas, orient=tk.VERTICAL, command=comics_canvas.yview)
+        scrollbar = tk.Scrollbar(comics_window, orient=tk.VERTICAL, command=comics_canvas.yview)
         scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
         comics_canvas.config(yscrollcommand=scrollbar.set)
 
@@ -278,12 +253,10 @@ class SearchView:
         # Configure the Canvas scrolling with the mouse wheel
         comics_canvas.bind_all('<MouseWheel>', lambda e: comics_canvas.yview_scroll(-1 * int(e.delta / 120), 'units'))
 
-        comics_canvas.yview_moveto(0.0)  # Ensure the scroll bar is at the top
+        comics_canvas.update_idletasks()  # Update to calculate the canvas size
+        comics_canvas.config(scrollregion=comics_canvas.bbox('all'))  # Set the initial scroll region
 
-        # end test
-        end = time.time()
-        round_time = round(end - start)
-        print(f"Runtime of the method is {round_time} seconds.")
+        comics_canvas.yview_moveto(0.0)  # Ensure the scroll bar is at the top
 
         comics_window.mainloop()
 
