89d32d7ea76b23f6921f1eff2fc378b7d6a81502
Nicolaus Bunch <77365237+Coldwel@users.noreply.github.com>
2024-04-02 19:50:51 -0700

    v0.1.6

--- main/main.py
+++ main/main.py
@@ -3,9 +3,11 @@ import json
 import tkinter as tk
 from controller import SearchController
 from view import SearchView
-from model import PUBLIC_KEY, PRIVATE_KEY
+from model import db_config
 
-SOFTWARE_VERSION = "0.1.4"
+
+SOFTWARE_VERSION = "0.1.6"
+
 
 with open("config.json") as f:
     config = json.load(f)
@@ -14,8 +16,8 @@ with open("config.json") as f:
 if __name__ == "__main__":
     # Create instances
     root = tk.Tk()
-    controller = SearchController(PUBLIC_KEY, PRIVATE_KEY)
-    version = SOFTWARE_VERSION  # Set version number
+    controller = SearchController(config, db_config)
+    version = SOFTWARE_VERSION
     app = SearchView(root, controller, version)
     root.geometry(window_size)
     root.mainloop()
